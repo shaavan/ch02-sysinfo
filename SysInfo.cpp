9bacf9aa9196d6bf4d4874f2e137cbf35f0e9c56
Shashwat <svangani239@gmail.com>
2021-10-27 19:49:40 +0530

    Transformed SysInfo file into Singleton
    - Done by making the constructor of the class protected. So that only
    the children classes are allowed to call constructor.
    - Made copy and assignment operator private so that to prevent copying
    of SysInfo instance.
    - Finally declared a static function instance that will return a
    reference to Sysinfo class.
    - Used #ifdef #endif QT Macros to ensure proper initialization of
    singleton instance for different OS.

--- SysInfo.cpp
+++ SysInfo.cpp
@@ -1,4 +1,23 @@
+#include <QtGlobal>
+
 #include "SysInfo.h"
+
+#ifdef Q_OS_WIN
+    #include "SysInfoWindowsImpl.h"
+#elif defined(Q_OS_LINUX)
+    #include "SysinfoLinuxImpl.h"
+#endif
+
+SysInfo& SysInfo::instance()
+{
+    #ifdef Q_OS_WIN
+        static SysinfoWindowsImpl singleton;
+    #elif defined(Q_OS_LINUX)
+        static SysinfoLinuxImpl singleton;
+    #endif
+
+    return singleton;
+}
 
 SysInfo::SysInfo()
 {
